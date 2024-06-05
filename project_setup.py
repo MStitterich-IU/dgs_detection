87410068bdf32835ab396a8c317d5be7995995be
Markus Stitterich <markus.stitterich@iu-study.org>
2024-06-05 18:51:50 +0200

    Added multicam support to project setup

--- project_setup.py
+++ project_setup.py
@@ -1,6 +1,6 @@
 import os
 
-def setupStructure(gestures, no_videos):
+def setupStructure(gestures, no_videos, multicam=False):
 
     # Folder for recorded NumPy data
     DATA_PATH = os.path.join('recording_data')
@@ -8,6 +8,8 @@ def setupStructure(gestures, no_videos):
     for gesture in gestures: 
         for video in range(1,no_videos+1):
             try:
+                if multicam:
+                    os.makedirs(os.path.join(DATA_PATH, gesture, str(video+no_videos)))
                 os.makedirs(os.path.join(DATA_PATH, gesture, str(video)))
                 
             except:
