d2a8bf270397b2a6d8d06adf93c2e8f91f43351f
Markus Stitterich <markus.stitterich@iu-study.org>
2024-06-06 18:53:09 +0200

    Adds Mediapipe keypoint detection

--- record_gestures.py
+++ record_gestures.py
@@ -1,18 +1,32 @@
 import cv2
+import mediapipe as mp
+
+holisticModel = mp.solutions.holistic
+mpDrawUtil = mp.solutions.drawing_utils
+
+def keypoint_detection(frame, model):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame.flags.writeable = False                  
+    results = model.process(frame)                 
+    frame.flags.writeable = True                    
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    return frame, results
 
 cameraCapture = cv2.VideoCapture(1)
+with holisticModel.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holisticModel:
+    while cameraCapture.isOpened():
+        # Read incoming camera frames
+        ret, frame = cameraCapture.read()
 
+        #Detect and process gesture keypoints
+        frame, results = keypoint_detection(frame, holisticModel)
 
-while cameraCapture.isOpened():
-    # Read incoming camera frames
-    ret, frame = cameraCapture.read()
+        # Show camera feed to user
+        cv2.imshow('OpenCV Feed', cv2.flip(frame, 180))
 
-    # Show camera feed to user
-    cv2.imshow('OpenCV Feed', cv2.flip(frame, 180))
-
-    # Quit capture gracefully via key input
-    key = cv2.waitKey(10)
-    if key == ord('q'):
-        break
+        # Quit capture gracefully via key input
+        key = cv2.waitKey(10)
+        if key == ord('q'):
+            break
 cameraCapture.release()
 cv2.destroyAllWindows()
