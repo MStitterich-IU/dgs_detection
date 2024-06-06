a44a63d814e196281564075ac8632c3b3a9a4506
Markus Stitterich <markus.stitterich@iu-study.org>
2024-06-06 19:32:20 +0200

    Adds further visualization for posture and both hands

--- record_gestures.py
+++ record_gestures.py
@@ -14,6 +14,9 @@ def keypoint_detection(frame, model):
 
 def visualize_keypoints(frame, results):
     mpDrawUtil.draw_landmarks(frame, results.face_landmarks, holisticModel.FACEMESH_CONTOURS ) # Visualize face contours
+    mpDrawUtil.draw_landmarks(frame, results.pose_landmarks, holisticModel.POSE_CONNECTIONS) # Posture connections
+    mpDrawUtil.draw_landmarks(frame, results.left_hand_landmarks, holisticModel.HAND_CONNECTIONS) # Left hand connections
+    mpDrawUtil.draw_landmarks(frame, results.right_hand_landmarks, holisticModel.HAND_CONNECTIONS) # Right hand connections
 
 cameraCapture = cv2.VideoCapture(1)
 with holisticModel.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hModel:
