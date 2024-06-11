d35dca2a703a78d7352e2722167ef1858b5c67d9
Markus Stitterich <markus.stitterich@iu-study.org>
2024-06-11 18:19:34 +0200

    Removes model eval and cleans up code

--- model_training.py
+++ model_training.py
@@ -15,15 +15,29 @@ framesPerVideo = len(os.listdir((os.path.join(pSetup.DATA_PATH, gestures[0], str
 labelMapping = {label:num for num, label in enumerate(gestures)}
 
 #Loading previously recorded data
-videos, labels = [], []
-for gesture in gestures:
-    for video in npy.array(os.listdir(os.path.join(pSetup.DATA_PATH, gesture))).astype(int):
-        frames = []
-        for frameNr in range(1, framesPerVideo+1):
-            res = npy.load(os.path.join(pSetup.DATA_PATH, gesture, str(video), "{}.npy".format(frameNr)))
-            frames.append(res)
-        videos.append(frames)
-        labels.append(labelMapping[gesture])
+def load_data():
+    videos, labels = [], []
+    for gesture in gestures:
+        for video in npy.array(os.listdir(os.path.join(pSetup.DATA_PATH, gesture))).astype(int):
+            frames = []
+            for frameNr in range(1, framesPerVideo+1):
+                res = npy.load(os.path.join(pSetup.DATA_PATH, gesture, str(video), "{}.npy".format(frameNr)))
+                frames.append(res)
+            videos.append(frames)
+            labels.append(labelMapping[gesture])
+    return videos, labels
+
+def setup_model():
+    model = Sequential()
+    model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+    model.add(LSTM(128, return_sequences=True, activation='relu'))
+    model.add(LSTM(64, return_sequences=False, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dense(npy.array(gestures).shape[0], activation='softmax'))
+    return model
+
+videos, labels = load_data()
 
 x = npy.array(videos)
 y = keras.utils.to_categorical(labels).astype(int)
@@ -32,30 +46,13 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
 
 log_dir = os.path.join('Logs')
 tensorBoardCB = TensorBoard(log_dir=log_dir)
-earlyStopCB = EarlyStopping("val_loss", patience=5, start_from_epoch=50)
+earlyStopCB = EarlyStopping("loss", patience=5, start_from_epoch=50)
 
-#clear_session()
+clear_session()
 keras.utils.set_random_seed(1)
 
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(npy.array(gestures).shape[0], activation='softmax'))
-
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, y_train, epochs=100, callbacks=[tensorBoardCB, earlyStopCB])
-
-#Make a prediction
-prediction = model.predict(X_test)
-for i in range(0,X_test.shape[0]):
-    pred = gestures[npy.argmax(prediction[i])]
-    sol = gestures[npy.argmax(y_test[i])]
-    if pred == sol:
-        print('Correct')
-    else:
-        print('Incorrect')
+model = setup_model()
+model.compile(optimizer='Adam', metrics=['categorical_accuracy'], loss='categorical_crossentropy')
+model.fit(X_train, y_train, epochs=80, callbacks=[tensorBoardCB, earlyStopCB])
 
 model.save('gesture_detection_model.keras')
