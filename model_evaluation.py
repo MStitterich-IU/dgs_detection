62d9f4344d83d5ebdeb1b6c560d3932b1ee5e1d6
Markus Stitterich <markus.stitterich@iu-study.org>
2024-06-14 13:09:21 +0200

    Changes model evaluation class to inherit from model class

--- model_evaluation.py
+++ model_evaluation.py
@@ -1,3 +1,4 @@
+from model_training import Model
 import numpy as npy
 import os
 import tkinter as tk
@@ -9,32 +10,10 @@ from keras.src.backend.common.global_state import clear_session
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
-class Model():
+class EvalModel(Model):
 
     def __init__(self, data_folder):
-        self.data_path = os.path.join(data_folder)
-        self.gestures = os.listdir(os.path.join(self.data_path))
-
-        self.model = Sequential()
-        self.model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
-        self.model.add(LSTM(128, return_sequences=True, activation='relu'))
-        self.model.add(LSTM(64, return_sequences=False, activation='relu'))
-        self.model.add(Dense(64, activation='relu'))
-        self.model.add(Dense(32, activation='relu'))
-        self.model.add(Dense(npy.array(self.gestures).shape[0], activation='softmax'))
-    
-    def loadData(self):
-        self.videos, self.labels = [], []
-        framesPerVideo = len(os.listdir((os.path.join(self.data_path, self.gestures[0], str(1)))))
-        labelMapping = {label:num for num, label in enumerate(self.gestures)}
-        for gesture in self.gestures:
-            for video in npy.array(os.listdir(os.path.join(self.data_path, gesture))).astype(int):
-                frames = []
-                for frameNr in range(1, framesPerVideo+1):
-                    res = npy.load(os.path.join(self.data_path, gesture, str(video), "{}.npy".format(frameNr)))
-                    frames.append(res)
-                self.videos.append(frames)
-                self.labels.append(labelMapping[gesture])
+        super().__init__(data_folder)
     
     def loadWeights(self):
         root = tk.Tk()
@@ -54,7 +33,7 @@ class Model():
 
 if __name__ == '__main__':
 
-    testModel = Model('testing_data')
+    testModel = EvalModel('testing_data')
     print(testModel.evaluate())
 
 
